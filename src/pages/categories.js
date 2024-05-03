ed013c5f5eb17ac4be7bc1d56791434c029e3acb
Grisha Angelov <grisha.angelov89@gmail.com>
2024-05-03 13:21:17 +0300

    add youtube category

---
+++ src/pages/categories.js
@@ -0,0 +1,174 @@
+export const categories = {
+  youtube: [
+    {
+      image:
+        "https://yt3.googleusercontent.com/4NapxEtLcHQ6wN2zA_DMmkOk47RFb_gy6sjSmUZGg_ARHjlIUjFsrNFddrcKMkTYpBNxCp3J=s160-c-k-c0x00ffffff-no-rj",
+      title: "Theo Browne (ex-Twitch)",
+      link: "https://www.youtube.com/@t3dotgg",
+    },
+    {
+      image:
+        "https://yt3.googleusercontent.com/ytc/AIdro_mLysKc36lc_FVk2j777olWvLOjgDz6NCNGdiQBnAKRENM=s160-c-k-c0x00ffffff-no-rj",
+      title: "Brad Traversy",
+      link: "https://www.youtube.com/@TraversyMedia",
+    },
+    {
+      image:
+        "https://yt3.googleusercontent.com/ytc/AIdro_k7J8-LWCa5QLDnY3x9kaArgJoSSMory4hgkYSBOFgvEg=s176-c-k-c0x00ffffff-no-rj",
+      title: "ThePrimeagen (ex-Netflix)",
+      link: "https://www.youtube.com/@ThePrimeTimeagen",
+    },
+    {
+      image:
+        "https://yt3.googleusercontent.com/ytc/AIdro_mQczyuvnXgEq8fApoOXpG2Yw_JKYqRA7kVOhNFuHLz9Vc=s160-c-k-c0x00ffffff-no-rj",
+      title: "Kevin Powell",
+      link: "https://www.youtube.com/@KevinPowell",
+    },
+    {
+      image:
+        "https://yt3.googleusercontent.com/ytc/AIdro_mUPDVdzH_xKT-q_hC1h2-7hJKd6GnKIY3Zydjv-SEmpc0=s176-c-k-c0x00ffffff-no-rj",
+      title: "Jack Herrington",
+      link: "https://www.youtube.com/@jherr/videos",
+    },
+    {
+      image:
+        "https://yt3.googleusercontent.com/ytc/AIdro_nFNsnkrn5f2X1v1ww_s1h93T3MEN7pTsPpJMMQBe68HsA=s176-c-k-c0x00ffffff-no-rj",
+      title: "Free Code Camp",
+      link: "https://www.youtube.com/@freecodecamp",
+    },
+    {
+      image:
+        "https://yt3.googleusercontent.com/ytc/AIdro_m9F60Dip2aA-2SkED8ssMmJ1oAiqU7JVevR8ZWKnqsNnU=s176-c-k-c0x00ffffff-no-rj-mo",
+      title: "NDC Conferences",
+      link: "https://www.youtube.com/@NDC",
+    },
+    {
+      image:
+        "https://yt3.googleusercontent.com/k31S2wNch7tS34F4au5tniAyX-BB1-xbtcS6ti6LRh6EaI5PkOsjCFqUUmeKQOZ4OcfSlTDBpg=s160-c-k-c0x00ffffff-no-rj",
+      title: "React Conf",
+      link: "https://www.youtube.com/@ReactConfOfficial",
+    },
+    {
+      image:
+        "https://i.ytimg.com/vi/E-50aGAZG38/hq720.jpg?sqp=-oaymwEXCK4FEIIDSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLDHhaxngvQHriwzXPqfcoIeBTS18g",
+      title: "React London Meetups",
+      link: "https://www.youtube.com/playlist?list=PLW6ORi0XZU0BL3Up9mXpP75ilJBDOjMsQ",
+    },
+    {
+      image:
+        "https://yt3.googleusercontent.com/YNL37Ul2RHQp9MCQkAOMFryfkvTGioUeJ6AVPS0LTpd-6ZMPVuF0nohS_0VL2GWwsP68u35Y9Tk=s176-c-k-c0x00ffffff-no-rj",
+      title: "React Conferences by GitNation",
+      link: "https://www.youtube.com/@ReactConferences",
+    },
+    {
+      image:
+        "https://yt3.googleusercontent.com/YNL37Ul2RHQp9MCQkAOMFryfkvTGioUeJ6AVPS0LTpd-6ZMPVuF0nohS_0VL2GWwsP68u35Y9Tk=s176-c-k-c0x00ffffff-no-rj",
+      title: "React Advanced London 2023",
+      link: "https://www.youtube.com/watch?v=hn_L56ypX1A",
+    },
+    {
+      image:
+        "https://yt3.googleusercontent.com/YNL37Ul2RHQp9MCQkAOMFryfkvTGioUeJ6AVPS0LTpd-6ZMPVuF0nohS_0VL2GWwsP68u35Y9Tk=s176-c-k-c0x00ffffff-no-rj",
+      title: "React Advanced London 2022",
+      link: "https://www.youtube.com/playlist?list=PLNBNS7NRGKMEi5oMDs4jRwiqIKbyc69MR",
+    },
+    {
+      image:
+        "https://yt3.googleusercontent.com/YNL37Ul2RHQp9MCQkAOMFryfkvTGioUeJ6AVPS0LTpd-6ZMPVuF0nohS_0VL2GWwsP68u35Y9Tk=s176-c-k-c0x00ffffff-no-rj",
+      title: "React Advanced London 2021",
+      link: "https://www.youtube.com/playlist?list=PLNBNS7NRGKMHk3w8wv7pBbBMUSjaplurj",
+    },
+    {
+      image:
+        "https://yt3.googleusercontent.com/YNL37Ul2RHQp9MCQkAOMFryfkvTGioUeJ6AVPS0LTpd-6ZMPVuF0nohS_0VL2GWwsP68u35Y9Tk=s176-c-k-c0x00ffffff-no-rj",
+      title: "React Advanced London 2019",
+      link: "https://www.youtube.com/playlist?list=PLNBNS7NRGKMH7yfpYQD4TrFV25SMOCIPM",
+    },
+    {
+      image:
+        "https://yt3.googleusercontent.com/YNL37Ul2RHQp9MCQkAOMFryfkvTGioUeJ6AVPS0LTpd-6ZMPVuF0nohS_0VL2GWwsP68u35Y9Tk=s176-c-k-c0x00ffffff-no-rj",
+      title: "React Summit 2023",
+      link: "https://www.youtube.com/playlist?list=PLNBNS7NRGKME880lcYdw9QwkCiDYQFgCK",
+    },
+    {
+      image:
+        "https://yt3.googleusercontent.com/YNL37Ul2RHQp9MCQkAOMFryfkvTGioUeJ6AVPS0LTpd-6ZMPVuF0nohS_0VL2GWwsP68u35Y9Tk=s176-c-k-c0x00ffffff-no-rj",
+      title: "React Summit 2022",
+      link: "https://www.youtube.com/playlist?list=PLNBNS7NRGKMGeJQWT_E6jwPEf5lcV1O-2",
+    },
+    {
+      image:
+        "https://yt3.googleusercontent.com/ytc/AIdro_ltRigbEjK9HfAhlU6gVdqYyHN7uulgrQfff_RXtLU0e-8=s176-c-k-c0x00ffffff-no-rj",
+      title: "JSConf",
+      link: "https://www.youtube.com/@jsconf_/videos",
+    },
+    {
+      image:
+        "https://yt3.googleusercontent.com/qgTWM66BUH2STKGd-jTl5DMoIZXnDhKpmo_Cyp3yBEEUwdYMeHvjrulSl1wioLz-rm1mNhg6VA=s176-c-k-c0x00ffffff-no-rj",
+      title: "GOTO Conferences",
+      link: "https://www.youtube.com/@GOTO-/videos",
+    },
+    {
+      image:
+        "https://yt3.googleusercontent.com/ezzZA1bOxrR4gm_Bg5SBroOZi8E7dyee6OGKY15fM-lxop-UVTlDfz_uCpY9QSZPostKNMxc=s176-c-k-c0x00ffffff-no-rj",
+      title: "JSWORLD Conference",
+      link: "https://www.youtube.com/@JSWORLDConference/videos",
+    },
+    {
+      image:
+        "https://yt3.googleusercontent.com/ezzZA1bOxrR4gm_Bg5SBroOZi8E7dyee6OGKY15fM-lxop-UVTlDfz_uCpY9QSZPostKNMxc=s176-c-k-c0x00ffffff-no-rj",
+      title: "React Miami 2023",
+      link: "https://www.youtube.com/playlist?list=PL02pdjMT4gWwIDHt4mzmzbiXU-7Sk1LN5",
+    },
+    {
+      image:
+        "https://yt3.googleusercontent.com/ezzZA1bOxrR4gm_Bg5SBroOZi8E7dyee6OGKY15fM-lxop-UVTlDfz_uCpY9QSZPostKNMxc=s176-c-k-c0x00ffffff-no-rj",
+      title: "React Miami 2022",
+      link: "https://www.youtube.com/playlist?list=PL02pdjMT4gWxC3sYlCBzPBPptb6eUvIZv",
+    },
+    {
+      image:
+        "https://yt3.googleusercontent.com/ytc/AIdro_ne03Xy-WJcYQHV7OYRy0FbGusuEzJy9ZsgJ6RWgJTNew=s176-c-k-c0x00ffffff-no-rj",
+      title: "Real World React",
+      link: "https://www.youtube.com/@RealWorldReact/videos",
+    },
+    {
+      image:
+        "https://yt3.googleusercontent.com/ytc/AIdro_ne03Xy-WJcYQHV7OYRy0FbGusuEzJy9ZsgJ6RWgJTNew=s176-c-k-c0x00ffffff-no-rj",
+      title: "Reactathon 2023",
+      link: "https://www.youtube.com/playlist?list=PLRvKvw42Rc7MoxLVE_9qM24NAjqDdbBJc",
+    },
+    {
+      image:
+        "https://yt3.googleusercontent.com/ytc/AIdro_ne03Xy-WJcYQHV7OYRy0FbGusuEzJy9ZsgJ6RWgJTNew=s176-c-k-c0x00ffffff-no-rj",
+      title: "Reactathon 2022",
+      link: "https://www.youtube.com/playlist?list=PLRvKvw42Rc7O0eWo2m_guXdZsGTEQM_jj",
+    },
+    {
+      image:
+        "https://yt3.googleusercontent.com/dSM3d9uWNRnqdRLlVXro0Sjb1UG2RBOJdk7zY0QvlDFbj91jVU0UlbqY7ZdnaVKqdjFiwxGbjZY=s160-c-k-c0x00ffffff-no-rj",
+      title: "ReactRally",
+      link: "https://www.youtube.com/@reactrally2196/videos",
+    },
+    {
+      image:
+        "https://yt3.googleusercontent.com/as8Rhow7_2_781OfflprsqqRKN4XTJwkh4IIqWTleZHmgiOsxAXgJe0vajxzFVHzcBSqQO6G=s160-c-k-c0x00ffffff-no-rj",
+      title: "Epic Web Dev",
+      link: "https://www.youtube.com/@EpicWebDev/videos",
+    },
+    {
+      image:
+        "https://yt3.googleusercontent.com/ytc/AIdro_k3yUmGX4HUMqqjtPUBUtevOGr6JNnU-PRxISzSUwQqtg=s176-c-k-c0x00ffffff-no-rj",
+      title: "devtools-fm",
+      link: "https://www.youtube.com/@devtoolsfm/videos",
+    },
+    {
+      image:
+        "https://yt3.googleusercontent.com/ytc/AIdro_mLnopXX-jYkH2ENEmuVwLf61NPhGobJmyBx-HaNB1a8_0=s176-c-k-c0x00ffffff-no-rj",
+      title: "Honeypot",
+      link: "https://www.youtube.com/@Honeypotio/videos",
+    },
+  ],
+};
+
+export const total = Object.values(categories).reduce((a, b) => a + b.length, 0);
